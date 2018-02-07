e244f95cce65ef039afdbf309928fd85d5ebd79a
Tom Morse <tom.morse@yale.edu>
2018-02-07 11:41:27 -0500

    Update from Ruben Tikidji-Hamburyan to implement a correction that was figured out through interactions with a modeler with questions. Through the conversation, they figured out that there is a discrepancy between formula in the paper and the actual code for Case study 2 in model. This version has a note in the readme about this and a correction in CaseStudy2-400HH/NEST/example.py

--- CaseStudy2-400HH/NEST/example-C.py
+++ CaseStudy2-400HH/NEST/example-C.py
@@ -8,9 +8,9 @@ import nest
 startbuild=time.time()
 
 nest.ResetKernel()
-nest.SetKernelStatus({"resolution": 0.05})
+#nest.SetKernelStatus({"resolution": 0.05, "print_time": False, "overwrite_files": True})
 
-vinit=[ x for x in np.genfromtxt("../volt.ssv") ]
+vinit=[ x for x in np.random.randn(400)*50.-50. ]
 nest.SetDefaults("hh_psc_alpha", { 'E_L':-30.3, 'tau_syn_in':1. } )
 neurons = nest.Create("hh_psc_alpha",400)
 for n in neurons:
@@ -20,10 +20,9 @@ nest.CopyModel("static_synapse","excitatory",{"weight":1.,  "delay":1.5})
 nest.CopyModel("static_synapse","inhibitory",{"weight":-300., "delay":3.0})
 
 conlist = list(np.genfromtxt("../connections.ssv").astype(int))
-conpairs = [([pre]*len(post), post) for pre, post in enumerate(conlist)]
-pre, post = zip(*conpairs)
-
-nest.Connect(np.array(neurons)[np.array(pre).flatten()], np.array(neurons)[np.array(post).flatten()], 'one_to_one', syn_spec="inhibitory")
+for pre,cs in enumerate(conlist):
+	for post in list(cs):
+		nest.Connect([neurons[pre]], [neurons[post]], syn_spec="inhibitory")
 
 ispikes  = nest.Create("spike_detector")
 nest.SetStatus(ispikes,[{"label": "hh-inhibitory",
@@ -41,8 +40,6 @@ endsimulate= time.time()
 
 print("Building time     : %.2f s"%(endbuild-startbuild ))
 print("Simulation time   : %.2f s"%(endsimulate-endbuild))
-print("Time step         : %.2f ms"%(nest.GetKernelStatus()['resolution']))
-
 
 ev = nest.GetStatus(ispikes, "events")[0]
 ispikes = np.dstack((ev["times"], ev["senders"]    ))[0]
